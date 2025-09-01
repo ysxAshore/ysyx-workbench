47a3499c07c8632e7d57c3c84d3625d541b39000
ysxAshore <19819660125@163.com>
2025-09-01 20:19:41 +0800

    [add]implemented the malloc and free in klib and passed the some games with character

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -5,41 +5,68 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
-int rand(void) {
+int rand(void)
+{
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+  return (unsigned int)(next / 65536) % 32768;
 }
 
-void srand(unsigned int seed) {
+void srand(unsigned int seed)
+{
   next = seed;
 }
 
-int abs(int x) {
+int abs(int x)
+{
   return (x < 0 ? -x : x);
 }
 
-int atoi(const char* nptr) {
+int atoi(const char *nptr)
+{
   int x = 0;
-  while (*nptr == ' ') { nptr ++; }
-  while (*nptr >= '0' && *nptr <= '9') {
+  while (*nptr == ' ')
+  {
+    nptr++;
+  }
+  while (*nptr >= '0' && *nptr <= '9')
+  {
     x = x * 10 + *nptr - '0';
-    nptr ++;
+    nptr++;
   }
   return x;
 }
 
-void *malloc(size_t size) {
+static uintptr_t hbrk;
+static bool firstCall = false;
+void *malloc(size_t size)
+{
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
-#endif
+  // extern Area heap;
+  // static uintptr_t hbrk = (uintptr_t)heap.start; 不能这么写 static变量的初始化只能来自于编译期常量 而heap.start是链接时常量
+  if (!firstCall)
+  {
+    extern Area heap;
+    hbrk = (uintptr_t)heap.start;
+    firstCall = true;
+  }
+
+  size = (size_t)ROUNDUP(size, 8);
+  char *old = (char *)hbrk;
+  hbrk += size;
+  assert((uintptr_t)heap.start <= hbrk && hbrk < (uintptr_t)heap.end); // 不超过heap范围
+  memset(old, 0, size);
+  return old;
+#else
   return NULL;
+#endif
 }
 
-void free(void *ptr) {
+void free(void *ptr)
+{
 }
 
 #endif
