0331c4fa56dfb316a8c1aaeb7ed37ff645afcc97
ysxAshore <19819660125@163.com>
2025-09-01 21:39:00 +0800

    [add]implemented the audio device nemu-am

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,26 +1,55 @@
 #include <am.h>
 #include <nemu.h>
 
-#define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
-#define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
-#define AUDIO_SAMPLES_ADDR   (AUDIO_ADDR + 0x08)
+#define AUDIO_FREQ_ADDR (AUDIO_ADDR + 0x00)
+#define AUDIO_CHANNELS_ADDR (AUDIO_ADDR + 0x04)
+#define AUDIO_SAMPLES_ADDR (AUDIO_ADDR + 0x08)
 #define AUDIO_SBUF_SIZE_ADDR (AUDIO_ADDR + 0x0c)
-#define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
-#define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
+#define AUDIO_INIT_ADDR (AUDIO_ADDR + 0x10)
+#define AUDIO_COUNT_ADDR (AUDIO_ADDR + 0x14)
 
-void __am_audio_init() {
+void __am_audio_init()
+{
 }
 
-void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+void __am_audio_config(AM_AUDIO_CONFIG_T *cfg)
+{
+  cfg->present = true; // 存在audio设备实现
+  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
-void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl)
+{
+  outl(AUDIO_FREQ_ADDR, ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+  outl(AUDIO_INIT_ADDR, 1); // 可以初始化了
 }
 
-void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+void __am_audio_status(AM_AUDIO_STATUS_T *stat)
+{
+  stat->count = inl(AUDIO_COUNT_ADDR); // 已用字节
 }
 
-void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+static size_t sbuf_point = 0; // 不能使用已用大小去定义指针 因为可能是从中间计算的
+
+void __am_audio_play(AM_AUDIO_PLAY_T *ctl)
+{
+  uint32_t sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
+  uint32_t size = ctl->buf.end - ctl->buf.start;
+  uint8_t *sbuf = (uint8_t *)AUDIO_SBUF_ADDR;
+  uint8_t *data = (uint8_t *)ctl->buf.start;
+  // 发完所有字节再返回
+  while (size--)
+  {
+    // 监测只要有一个空余就可以填充
+    while (sbuf_size - inl(AUDIO_COUNT_ADDR) < 1)
+      ;
+    sbuf[sbuf_point++] = *(data++);
+    // 迭代addr data
+    if (sbuf_point >= sbuf_size) // 循环缓冲区
+      sbuf_point = 0;
+
+    outl(AUDIO_COUNT_ADDR, inl(AUDIO_COUNT_ADDR) + 1); // 更新已用大小
+  }
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -1,23 +1,24 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <common.h>
 #include <device/map.h>
 #include <SDL2/SDL.h>
 
-enum {
+enum
+{
   reg_freq,
   reg_channels,
   reg_samples,
@@ -29,19 +30,68 @@ enum {
 
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
+static int sbuf_point = 0;
 
-static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+static void audio_callback(void *userdata, Uint8 *stream, int len)
+{
+  // 初始化音频数据为静音
+  SDL_memset(stream, 0, len);
+
+  // 获取缓冲区长度 取缓冲区已用长度和len中的最小值
+  uint32_t useSize = audio_base[reg_count];
+  uint32_t size = useSize < len ? useSize : len;
+
+  if (sbuf_point + size > CONFIG_SB_SIZE)
+  {
+    SDL_MixAudio(stream, sbuf + sbuf_point, CONFIG_SB_SIZE - sbuf_point, SDL_MIX_MAXVOLUME);
+    SDL_MixAudio(stream + CONFIG_SB_SIZE - sbuf_point, sbuf, size - CONFIG_SB_SIZE + sbuf_point, SDL_MIX_MAXVOLUME);
+    // sbuf_point = (size - CONFIG_SB_SIZE + sbuf_point) % CONFIG_SB_SIZE;
+  }
+  else
+  {
+    SDL_MixAudio(stream, sbuf + sbuf_point, size, SDL_MIX_MAXVOLUME);
+    // sbuf_point = (sbuf_point + size) % CONFIG_SB_SIZE; //这里应该是需要%CONFIG_SIZE
+  }
+  sbuf_point = (sbuf_point + size) % CONFIG_SB_SIZE;
+  audio_base[reg_count] -= size; // 播放完了可以释放空间
 }
 
-void init_audio() {
+static void init_SDL()
+{
+  SDL_AudioSpec s = {};
+  s.format = AUDIO_S16SYS;
+  s.userdata = NULL;
+  s.channels = audio_base[reg_channels];
+  s.samples = audio_base[reg_samples];
+  s.freq = audio_base[reg_freq];
+  s.callback = audio_callback;
+
+  SDL_InitSubSystem(SDL_INIT_AUDIO);
+  SDL_OpenAudio(&s, NULL);
+  SDL_PauseAudio(0);
+}
+
+static void audio_io_handler(uint32_t offset, int len, bool is_write)
+{
+  if (audio_base[reg_init])
+  {
+    init_SDL();
+    audio_base[reg_init] = 0;
+  }
+}
+
+void init_audio()
+{
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
   audio_base = (uint32_t *)new_space(space_size);
 #ifdef CONFIG_HAS_PORT_IO
-  add_pio_map ("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
+  add_pio_map("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
 #else
   add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size, audio_io_handler);
 #endif
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+
+  audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
 }
