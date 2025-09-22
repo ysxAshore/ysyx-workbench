39b12a8ce1f793aa523c9104fb23bf41f5b6850f
ysxAshore <19819660125@163.com>
2025-09-22 22:29:04 +0800

    [add]implemented the bitrev module based spi and passed the bitrev_test

---
+++ am-kernels/tests/am-tests/src/tests/bitrev_test.c
@@ -0,0 +1,40 @@
+#include <amtest.h>
+#define SPI_BASE 0x10001000
+#define SPI_REG_TX (SPI_BASE + 0x0)
+#define SPI_REG_RX (SPI_BASE + 0x0)
+#define SPI_REG_CTRL (SPI_BASE + 0x10)
+#define SPI_REG_DIV (SPI_BASE + 0x14)
+#define SPI_REG_SS (SPI_BASE + 0x18)
+
+int main(const char *args) {
+	int32_t a = 0x57;
+	
+	// 1. 设置TX发送数据
+	*(volatile uint32_t *)SPI_REG_TX = a;
+	
+	// 2. 设置 SCK 分频（尽可能快）不过仿真没关系
+    *(volatile uint32_t *)SPI_REG_DIV = 1;
+
+	// 3. 设置 slave 号为 7
+	*(volatile uint32_t *)SPI_REG_SS = 1 << 7;
+	
+	// 4. 设置CTRL 
+	// CHAR_LEN = 10
+	// GO_BSY = 1
+	// Rx_NEG:0 上升沿锁存miso
+	// Tx_NEG:1 上降沿发送mosi
+	// LSB=1
+	// IE=0
+	// ASS=1
+	*(volatile uint32_t *)SPI_REG_CTRL = 0x10 | 0x1 << 8 | 0x5 << 11;
+
+	// 5. 检测GO_BSY是否为0
+	while(*(volatile uint32_t *)SPI_REG_CTRL & 0x1 << 8);
+
+	// 6. 读出数据Rx
+	uint16_t rx_data = *(volatile uint32_t *)SPI_REG_RX;
+
+    panic_on(0xea != (rx_data >> 8), "error");
+
+	return 0;
+}
--- ysyxSoC/perip/bitrev/bitrev.v
+++ ysyxSoC/perip/bitrev/bitrev.v
@@ -1,8 +1,39 @@
 module bitrev (
-  input  sck,
-  input  ss,
-  input  mosi,
-  output miso
+  input  wire       ss,     // chip select (active low)
+  input  wire       sck,    // spi clock
+  input  wire       mosi,   // master out slave in
+  output reg        miso    // master in slave out
 );
-  assign miso = 1'b1;
+  //  assign miso = 1'b1;
+  reg [3:0] bit_cnt; // 记录目前收到了几个bit
+  reg [7:0] shift_in;
+  reg [7:0] shift_out;
+
+  // 上升沿采样 下降沿输出
+  // posedge ss 异步复位
+  // 这样每次传输结束后 都会恢复到默认状态
+  always @(posedge sck or posedge ss) begin
+	  if(ss) begin
+		bit_cnt <= 'b0;
+		shift_in <= 'b0;
+		shift_out <= 'hff;
+	  end else begin
+		shift_in <= {shift_in[6:0], mosi}; //放最低位了
+		shift_out <= {shift_out[6:0], 1'b0};
+		bit_cnt <= bit_cnt + 1;
+		if(bit_cnt == 'h7) begin
+			shift_out <= {mosi, shift_in[0], shift_in[1], shift_in[2], shift_in[3], shift_in[4],
+						  shift_in[5], shift_in[6]};
+			bit_cnt <= 'h0;
+		end
+	  end
+  end
+
+  always @(negedge sck or posedge ss) begin
+	if(ss) miso <= 'b1;
+	else begin
+		miso <= shift_out[7];
+	end
+  end
+
 endmodule
